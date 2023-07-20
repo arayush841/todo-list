1d0bff96175b8d2305898aa65d6bcbd6125e6979
Ayush <arayush841@gmail.com>
2023-07-20 21:23:50 +0530

    final commit

---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,20 @@
+import { ITask } from "../Interface";
+
+interface IProps {
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
+}
+
+const TodoTask = ({ task, completeTask }: IProps) => {
+  return (
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button onClick={() => completeTask(task.taskName)}>X</button>
+    </div>
+  );
+};
+
+export default TodoTask;
